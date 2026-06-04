f667fe6ad1fb3291a24fcc4856db9f37cb128d35
adityanimkar137 <adityanimkar2004@gmail.com@gmail.com>
2026-06-04 15:25:43 +0530

    Bacis Folders

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public CardData data;
+}
---
+++ Assets/Scripts/Data/CardData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CardData
+{
+    public string cardName;
+    public int gritCost;
+    public string cardType;
+    public string description;
+    public int effectValue;
+}
