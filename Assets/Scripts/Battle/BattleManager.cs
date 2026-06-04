a7f97f4ad7b714f65325c19671a4b833fb46d45d
adityanimkar137 <adityanimkar2004@gmail.com@gmail.com>
2026-06-04 16:44:55 +0530

    Step 1.2 UI

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -9,14 +9,35 @@ public class BattleManager : MonoBehaviour
     public int enemyHP = 100;
     public int grit = 3;
     public int turn = 1;
-
+    public Card[] allCards;
     public List<Card> hand = new List<Card>();
 
     void Start()
     {
-        Debug.Log("Battle started!");
+    if (allCards == null || allCards.Length == 0)
+    {
+        Debug.LogWarning("No cards assigned to All Cards!");
+        return;
     }
 
+    Debug.Log("Loading " + allCards.Length + " cards");
+    DrawCards(5);
+    } 
+   public void DrawCards(int count)
+    {
+    for (int i = 0; i < count; i++)
+        {
+        if (allCards.Length == 0)
+            break;
+
+        Card randomCard =
+            allCards[Random.Range(0, allCards.Length)];
+
+        hand.Add(randomCard);
+
+        Debug.Log("Drew: " + randomCard.data.cardName);
+        }
+    }
     void Update()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
@@ -26,9 +47,16 @@ public class BattleManager : MonoBehaviour
     }
 
     public void EndTurn()
-    {
-        turn++;
-        grit = 3;
-        Debug.Log("Turn " + turn);
-    }
+{
+    turn++;
+
+    grit = 3;
+
+    hand.Clear();
+
+    DrawCards(2);
+
+    Debug.Log("Turn " + turn);
+    Debug.Log("Enemy HP: " + enemyHP);
+}
 }
